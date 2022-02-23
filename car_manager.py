561df867d3fbccf1f0e9ffc1271edc19638e7d0d
Buzato <joaobuzato@gmail.com>
2022-02-23 20:26:52 -0300

    adding play class and behavior and car manager and behavior

---
+++ car_manager.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+import random
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+LANES_Y = [-280, -240, -200, -160, -120, -80, -40, 0, 40, 80, 280, 240, 200, 160, 120]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+cars = []
+
+class CarManager:
+    def __init__(self):
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def generate_car(self):
+        # it is a random generation of cars, i generates a car for each 3 updates of the screen
+        if random.randint(1, 4) == 3:
+            car = Turtle()
+            car.penup()
+            car.setheading(180)
+            car.color(random.choice(COLORS))
+            car.shape("square")
+            car.shapesize(stretch_len=2)
+            car.goto(300, random.choice(LANES_Y))
+            cars.append(car)
+
+    def move_cars(self):
+        for car in cars:
+            if car.xcor() < -320:
+                cars.remove(car)
+                del car
+            else:
+                car.forward(self.car_speed)
+
+    def increase_speed(self):
+        self.car_speed += 10
+
+    def reset_speed(self):
+        self.car_speed = STARTING_MOVE_DISTANCE
---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+car_manager = CarManager()
+player = Player()
+
+screen.listen()
+screen.onkey(key="Up", fun=player.move)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    player.check_finish()
+    car_manager.generate_car()
+    car_manager.move_cars()
+    screen.update()
